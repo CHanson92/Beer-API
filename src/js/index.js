ef31c7916e7a555b8bf95021e03094f1d028a3ea
Chris Hanson <BlondyHanson@gmail.com>
2018-11-06 16:09:32 +0000

    Commit 4

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+// Constructor and Ajax function
+
 class beers {
     constructor(name, description, imageUrl, abv, ibu, ph) {
         this.name = name;
@@ -8,16 +10,15 @@ class beers {
         this.ph = ph;
     }
 }
-
-$(function() {
-    const url = 'https://api.punkapi.com/v2/beers?';
+$('footer').on('click', 'button', function() {
+    $('main').empty();
+    let url = ('https://api.punkapi.com/v2/beers?page=' + $(this).text() + '&per_page=6')
     $.ajax({
         url: url,
         method: 'GET',
       }).done(function(result) {
         let beers = [];
-        console.log(result);
-        for(i = 0; i < 6; i++) {
+        for(i = 0; i < result.length; i++) {
             let $name = result[i].name;
             let $description = result[i].description;
             let $imageUrl = result[i].image_url;
@@ -27,8 +28,40 @@ $(function() {
             let $section = $('<section class="beers"></section>')
             let $ul = $('<ul></ul>')
             $section.append('<img src="'+$imageUrl+'" />')
-            $section.append('<h1>"'+$name+'"</h1>')
-            $section.append('<p>"'+$description+'"</p>')
+            $section.append('<h1>'+$name+'</h1>')
+            $section.append('<p>'+$description+'</p>')
+            $ul.append('<li>ABV<span>'+$abv+'</span></li>')
+            $ul.append('<li>IBU<span>'+$ibu+'</span></li>')
+            $ul.append('<li>pH<span>'+$ph+'</span></li>')
+            $section.append($ul)
+            $('main').append($section)
+        }
+
+    }).fail(function(err) {
+        throw err;
+
+      });
+});
+
+$(function() {
+    const url = 'https://api.punkapi.com/v2/beers?page=1&per_page=6';
+    $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function(result) {
+        let beers = [];
+        for(i = 0; i < result.length; i++) {
+            let $name = result[i].name;
+            let $description = result[i].description;
+            let $imageUrl = result[i].image_url;
+            let $abv = result[i].abv;
+            let $ibu = result[i].ibu;
+            let $ph = result[i].ph;
+            let $section = $('<section class="beers"></section>')
+            let $ul = $('<ul></ul>')
+            $section.append('<img src="'+$imageUrl+'" />')
+            $section.append('<h1>'+$name+'</h1>')
+            $section.append('<p>'+$description+'</p>')
             $ul.append('<li>ABV<span>'+$abv+'</span></li>')
             $ul.append('<li>IBU<span>'+$ibu+'</span></li>')
             $ul.append('<li>pH<span>'+$ph+'</span></li>')
@@ -41,3 +74,5 @@ $(function() {
 
       });
 }) 
+
+// JQuery Paging Plugin
