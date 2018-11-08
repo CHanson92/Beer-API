79203b373840bc22cf7a0057be15c5bb2a264a62
Chris Hanson <BlondyHanson@gmail.com>
2018-11-08 11:54:55 +0000

    Commit 7

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@
 homepage();
 
 class beers {
-    constructor(name, tagline, description, imageUrl, abv, ibu, ph) {
+    constructor(name, tagline, description, imageUrl, abv, ibu, ph, ingredients, malt, 
+        hops, yeast) {
         this.name = name;
         this.tagline = tagline;
         this.description = description;
@@ -10,6 +11,10 @@ class beers {
         this.abv = abv;
         this.ibu = ibu;
         this.ph = ph;
+        this.ingredients = ingredients;
+        this.malt = malt;
+        this.hops = hops;
+        this.yeast = yeast;
     }
 }
 
@@ -21,6 +26,9 @@ $('nav').on('click', '.listofbeers', function() {
 
 // Ajax for bringing beers from API into webpage
 function homepage() {
+    $('aside').hide();
+    $('main').show();
+    $('footer').show();
     const url = 'https://api.punkapi.com/v2/beers?page=1&per_page=6';
     $.ajax({
         url: url,
@@ -29,30 +37,29 @@ function homepage() {
         $('main').empty();
         let beers = [];
         for(i = 0; i < result.length; i++) {
-            let $name = result[i].name;
-            let $description = result[i].description;
-            let $imageUrl = result[i].image_url;
-            let $abv = result[i].abv;
-            let $ibu = result[i].ibu;
-            let $ph = result[i].ph;
+            let name = result[i].name;
+            let description = result[i].description;
+            let imageUrl = result[i].image_url;
+            let abv = result[i].abv;
+            let ibu = result[i].ibu;
+            let ph = result[i].ph;
             let $section = $('<section class="beers"></section>')
             let $ul = $('<ul></ul>')
-            $section.append('<img src="'+$imageUrl+'" />')
-            $section.append('<h1>'+$name+'</h1>')
-            $section.append('<p>'+$description+'</p>')
-            $ul.append('<li>ABV<span>'+$abv+'</span></li>')
-            $ul.append('<li>IBU<span>'+$ibu+'</span></li>')
-            $ul.append('<li class="pH">pH<span>'+$ph+'</span></li>')
+            let $pH = $('<li class="pH">pH<span>'+ph+'</span></li>')
+            $section.append('<img src="'+imageUrl+'" />')
+            $section.append('<h1>'+name+'</h1>')
+            $section.append('<p>'+description+'</p>')
+            $ul.append('<li>ABV<span>'+abv+'</span></li>')
+            $ul.append('<li>IBU<span>'+ibu+'</span></li>')
+            $ul.append($pH)
+            if(ph > 1 && ph < 5){
+                $pH.css('background-color','orange');
+                } else if (ph > 5 && ph < 7){
+                $pH.css('background-color','red');
+                } else {$pH.css('background-color', 'blue');
+                }
             $section.append($ul)
             $('main').append($section)
-
-            if($ph > 1 && $ph < 5) {
-                $(".pH").css('background-color','orange');
-            } else if ($ph > 5 && $ph < 7) {
-                $(".pH").css('background-color', 'red');
-            } else {
-                $(".pH").css('background-color', 'blue');
-        }
     }
 
     }).fail(function(err) {
@@ -65,6 +72,9 @@ function homepage() {
 
 $('footer').on('click', 'button', function() {
     $('main').empty();
+    $('aside').hide();
+    $('main').show();
+    $('footer').show();
     let url = ('https://api.punkapi.com/v2/beers?page=' + $(this).text() + '&per_page=6')
     $.ajax({
         url: url,
@@ -72,30 +82,29 @@ $('footer').on('click', 'button', function() {
       }).done(function(result) {
         let beers = [];
         for(i = 0; i < result.length; i++) {
-            let $name = result[i].name;
-            let $description = result[i].description;
-            let $imageUrl = result[i].image_url;
-            let $abv = result[i].abv;
-            let $ibu = result[i].ibu;
-            let $ph = result[i].ph;
+            let name = result[i].name;
+            let description = result[i].description;
+            let imageUrl = result[i].image_url;
+            let abv = result[i].abv;
+            let ibu = result[i].ibu;
+            let ph = result[i].ph;
             let $section = $('<section class="beers"></section>')
             let $ul = $('<ul></ul>')
-            $section.append('<img src="'+$imageUrl+'" />')
-            $section.append('<h1>'+$name+'</h1>')
-            $section.append('<p>'+$description+'</p>')
-            $ul.append('<li>ABV<span>'+$abv+'</span></li>')
-            $ul.append('<li>IBU<span>'+$ibu+'</span></li>')
-            $ul.append('<li class="pH">pH<span>'+$ph+'</span></li>')
+            let $pH = $('<li class="pH">pH<span>'+ph+'</span></li>')
+            $section.append('<img src="'+imageUrl+'" />')
+            $section.append('<h1>'+name+'</h1>')
+            $section.append('<p>'+description+'</p>')
+            $ul.append('<li>ABV<span>'+abv+'</span></li>')
+            $ul.append('<li>IBU<span>'+ibu+'</span></li>')
+            $ul.append($pH)
+            if(ph > 1 && ph < 5){
+            $pH.css('background-color','orange');
+            } else if (ph > 5 && ph < 7){
+            $pH.css('background-color','red');
+            } else {$pH.css('background-color', 'blue');
+            }
             $section.append($ul)
             $('main').append($section)
-
-            if($ph > 1 && $ph < 5){
-                $(".pH").css('background-color','orange');
-            } else if ($ph > 6 && $ph < 7){
-                $(".pH").css('background-color','red');
-            } else {
-                $(".pH").css('background-color', 'blue');
-            }
         }
 
     }).fail(function(err) {
@@ -104,51 +113,57 @@ $('footer').on('click', 'button', function() {
       });
 });
 
-// Ajax for Quick Find Beer
-
-
+//Quick Find Button
 $('nav').on('click', '.quick-find', function() {
+    randombeer();
+});
+
+// Ajax for Quick Find Beer
+function randombeer() {
+    $('main').hide();
+    $('aside').show();
+    $('footer').hide();
     const url = 'https://api.punkapi.com/v2/beers/random';
     $.ajax({
         url: url,
         method: 'GET',
       }).done(function(result) {
         $('main').empty();
+        $('aside').empty();
         let beers = [];
         for(i = 0; i < result.length; i++) {
-            let $name = result[i].name;
-            let $tagline = result[i].tagline;
-            let $description = result[i].description;
-            let $imageUrl = result[i].image_url;
-            let $abv = result[i].abv;
-            let $ibu = result[i].ibu;
-            let $ph = result[i].ph;
+            let name = result[i].name;
+            let tagline = result[i].tagline;
+            let description = result[i].description;
+            let imageUrl = result[i].image_url;
+            let abv = result[i].abv;
+            let ibu = result[i].ibu;
+            let ph = result[i].ph;
             let $section = $('<section class="random-beer"></section>')
-            let $section1 = $('<section class="findmeanotherbeer"></section>')
+            let $section1 = $('<section class="randombeerinfo"></section>')
             let $section2 = $('<section class="givemeanotherbeer"></section>')
             let $ul = $('<ul></ul>')
-            $section.append('<img src="'+$imageUrl+'" />')
-            $section1.append('<h1>'+$name+'</h1>')
-            $section1.append('<h2>'+$tagline+'</h2>')
-            $section1.append('<p>'+$description+'</p>')
-            $ul.append('<li>ABV<span>'+$abv+'</span></li>')
-            $ul.append('<li>IBU<span>'+$ibu+'</span></li>')
-            $ul.append('<li class="pH">pH<span>'+$ph+'</span></li>')
+            let $pH = $('<li class="pH">pH<span>'+ph+'</span></li>')
+            $section.append('<img src="'+imageUrl+'" />')
+            $section1.append('<h1>'+name+'</h1>')
+            $section1.append('<h2>'+tagline+'</h2>')
+            $section1.append('<p>'+description+'</p>')
+            $ul.append('<li>ABV<span>'+abv+'</span></li>')
+            $ul.append('<li>IBU<span>'+ibu+'</span></li>')
+            $ul.append($pH)
+            if(ph > 1 && ph < 5) {
+                $($pH).css('background-color','orange');
+            } else if(ph > 5 && ph < 7) {
+                $($pH).css('background-color', 'red');
+            } else {
+                $($pH).css('background-color', 'blue');
+                }
             $section1.append($ul)
             $section1.append('<button class="ingredients">INGREDIENTS</button>')
             $section2.append('<button class="givemeanotherbeerbutton">GIVE ME ANOTHER BEER</button>')
-            $('footer').remove()
-            $('main').append($section)
-            $('main').append($section1)
-            $('main').append($section2)
-
-            if($ph > 1 && $ph < 5) {
-                $(".pH").css('background-color','orange');
-            } else if($ph > 5 && $ph < 7) {
-                $(".pH").css('background-color', 'red');
-            } else {
-                $(".pH").css('background-color', 'blue');
-                }
+            $('aside').append($section)
+            $('aside').append($section1)
+            $('aside').append($section2)
             }
         }
 
@@ -156,4 +171,12 @@ $('nav').on('click', '.quick-find', function() {
         throw err;
 
       })
-    });
+    };
+// Function for clicking 'Give me another beer' button
+
+$('aside').on('click', '.givemeanotherbeerbutton', function() {
+    randombeer();
+});
+
+// Function for Ingredients Modal
+
